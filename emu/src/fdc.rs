283f7b05da9f8bba28df39beaa0f3c75732addfa
Scott LaVigne <lavignes@amazon.com>
2023-07-14 21:25:40 -0700

    Add FDC flags

--- emu/src/fdc.rs
+++ emu/src/fdc.rs
@@ -6,7 +6,48 @@ use crate::bus::BusDevice;
 
 enum StatusFlags {}
 
+impl StatusFlags {
+    const BUSY: u8 = 1 << 0;
+
+    const DRQ: u8 = 1 << 1;
+    const INDEX: u8 = 1 << 1;
+
+    const LOST_DATA: u8 = 1 << 2;
+    const TRACK_0: u8 = 1 << 2;
+
+    const CRC_ERROR: u8 = 1 << 3;
+
+    const RECORD_NOT_FOUND: u8 = 1 << 4;
+    const SEEK_ERROR: u8 = 1 << 4;
+
+    const WRITE_FAULT: u8 = 1 << 5;
+    const RECORD_TYPE: u8 = 1 << 5;
+    const HEAD_LOADED: u8 = 1 << 5;
+
+    const WRITE_PROTECT: u8 = 1 << 6;
+
+    const NOT_READY: u8 = 1 << 7;
+}
+
 enum CommandFlags {}
+
+impl CommandFlags {
+    const STEPPING_MOTOR_RATE_MASK: u8 = 0b0000_0011;
+    const VERIFY: u8 = 1 << 2;
+    const HEAD_LOAD: u8 = 1 << 3;
+    const UPDATE_TRACK: u8 = 1 << 4;
+
+    const SIDE_COMPARE: u8 = 1 << 1;
+    const DELAY: u8 = 1 << 2;
+    const SIDE_SELECT: u8 = 1 << 3;
+    const MULTIPLE_RECORD: u8 = 1 << 4;
+    const DATA_ADDRESS_MARK: u8 = 1 << 0;
+
+    const INTERRUPT_NOT_READY_TO_READY: u8 = 1 << 0;
+    const INTERRUPT_READY_TO_NOT_READY: u8 = 1 << 1;
+    const INTERRUPT_INDEX_PULSE: u8 = 1 << 2;
+    const INTERRUPT_IMMEDIATE: u8 = 1 << 3;
+}
 
 pub struct Fdc<T> {
     handle: T,
